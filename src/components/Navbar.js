abddf3d68e3cfaf453179124b8f7f6cad8720950
Suweka <sansilunijas.22@uom.lk>
2025-01-05 19:13:21 +0530

    Implemented Navbar styles and logic

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css"; // Import CSS for Navbar styles
+import newLogo from "../assets/new-logo.png"; // Import logo
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
+    <nav className="navbar">
+      <div className="logo-section">
+        <img src={newLogo} alt="EduSphere Logo" className="navbar-logo" />
+      </div>
+      <ul className="nav-links">
+        <li><Link to="/" className="active">Home</Link></li>
         <li><Link to="/students">Students</Link></li>
         <li><Link to="/courses">Courses</Link></li>
         <li><Link to="/enrollments">Enrollments</Link></li>
         <li><Link to="/profile">Profile</Link></li>
       </ul>
+      <div className="profile-icon">
+        <img src={require("../assets/profile-icon.png")} alt="Profile" />
+      </div>
     </nav>
   );
 };
